4941edb59c7a1d862cf683de8cb544f89a17575f
Marine Mignot <mignomarine@gmail.com>
2024-10-18 00:42:15 +0200

    app

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -1,7 +1,5 @@
 import { Component, inject } from '@angular/core';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
-import { datasService } from '../datas.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-formulaire',
@@ -9,8 +7,22 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './formulaire.component.scss'
 })
 export class FormulaireComponent {
-  email(){
-    alert("Message envoyé");
-  }; 
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_5id4fcn', 'template_nhkdcg9', e.target as HTMLFormElement, {
+        publicKey: 'ic6YSvJuTWnMUU3AY',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert("Message envoyé !")
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 
 }
